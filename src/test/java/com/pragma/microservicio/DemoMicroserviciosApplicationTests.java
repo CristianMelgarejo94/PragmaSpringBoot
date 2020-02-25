5f24c6e0b7b01e9656f27e65243d86832047d54d
Cristian <cristancho1994@hotmail.com>
2020-02-25 00:04:44 -0500

    Primer commit

---
+++ src/test/java/com/pragma/microservicio/DemoMicroserviciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.microservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMicroserviciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
